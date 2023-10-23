46c2a0579c4a941e6e42b93074064b0732d75049
erison <erison@erison-HP-Laptop-15-bs1xx>
2023-10-23 12:47:08 -0300

    ACTUAL

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -3,11 +3,21 @@
 ################################################################################
 
 ASM_SRCS := 
+C++_SRCS := 
+CC_SRCS := 
+CPP_SRCS := 
+CXX_SRCS := 
 C_SRCS := 
+C_UPPER_SRCS := 
 OBJ_SRCS := 
 O_SRCS := 
 S_UPPER_SRCS := 
+C++_DEPS := 
+CC_DEPS := 
+CPP_DEPS := 
+CXX_DEPS := 
 C_DEPS := 
+C_UPPER_DEPS := 
 EXECUTABLES := 
 OBJS := 
 
